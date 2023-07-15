bfedcfca52d21e6286bccbb4c9db8551c5910960
IuriiKarpunov <karpunov.yury@yandex.ru>
2023-07-15 16:16:08 +0400

    сверстал экран для показа одного изображения

---
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Iurii on 15.07.23.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
